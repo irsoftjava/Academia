86036a21a644446f80fd3c609eb4a315d6af7a3c
irsoft <ivanrsaiz@gmail.com>
2023-09-30 16:12:33 +0100

    Se actualizó el paquete v2 de loteria.

--- src/main/java/com/pro/investigacion/loteria/v2/Bonoloto.java
+++ src/main/java/com/pro/investigacion/loteria/v2/Bonoloto.java
@@ -1,4 +1,11 @@
 package com.pro.investigacion.loteria.v2;
 
-public record Bonoloto(int day, int month, int year, int[] bet, int complementario, int reintegro) {
+import java.util.Arrays;
+
+public record Bonoloto(int day, int month, int year, Integer[] bet, int complementario, int reintegro) {
+
+    @Override
+    public String toString() {
+        return "Bonoloto(" + day + ", " + month + ", " + year + ", " + Arrays.toString(bet) + ", " + complementario + ", " + reintegro + ')';
+    }
 }
